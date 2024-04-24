aaffa592b55a88bccf61a7688beab0d6292241ca
zynioz2 <167985323+zynioz2@users.noreply.github.com>
2024-04-24 14:16:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
